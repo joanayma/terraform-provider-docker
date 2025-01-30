1155aceb56ff1704dc581571dd989b17285cb88e
Joan Aymà <joan.ayma@clarivate.com>
2025-01-30 09:03:55 +0100

    test: fixes on acceptance tests

--- testdata/resources/docker_image/testAccDockerImageName.tf
+++ testdata/resources/docker_image/testAccDockerImageName.tf
@@ -6,7 +6,8 @@ resource "docker_container" "foo" {
   depends_on = [
     docker_image.ubuntu
   ]
-  image   = docker_image.ubuntu.image_id
-  name    = "foobar"
-  command = ["sh", "-c", "while true ;do wait ;done"]
+  network_mode = "bridge"
+  image        = docker_image.ubuntu.image_id
+  name         = "foobar"
+  command      = ["sh", "-c", "while true ;do wait ;done"]
 }
